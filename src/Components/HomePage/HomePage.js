8de28a71ad22ca75be90d0a5f6cc0dec0bb6ef01
githubchalatahu <77007044+githubchalatahu@users.noreply.github.com>
2021-12-29 08:19:52 +0530

    Add files via upload

---
+++ src/Components/HomePage/HomePage.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Navbar from "./Sections/Navbar";
+import Banner from "./Sections/Banner";
+import KnowAbout from "./Sections/KnowAbout";
+
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Banner />
+        <KnowAbout />
+        
+      </div>
+    );
+  }
+}
+
+export default HomePage;
